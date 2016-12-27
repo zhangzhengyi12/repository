bbbb9c1e9a6368c3ab567ff516d0a8cef06c8b84
zhangzhengyi12 <zhangzhengyi12@live.com>
2016-12-27 22:00:11 +0800

    rmainfile

--- readme.txt
+++
@@ -1,4 +0,0 @@
-Git diff a version control system.
-Git is free software.
-Creating a new branch is quick.
-creathaha
---
+++ responsive02/src/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        auto:true,
+        stopAutoOnClick:true,
+        autoHover: true,
+        pause:2000
+
+    });
+});
